dc6cc27b04c49fb6ce43347d8f4d32c535c16df7
Gloory1 <167514139+Gloory1@users.noreply.github.com>
2026-07-25 21:01:51 +0300

    Add files via upload

---
+++ ota_v5.5.2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Starting OTA Update to v5.5.2..."
+rm -rf /tmp/superwifi-bash*
+cd /tmp || exit 1
+wget -qO superwifi-bash-v5.5.2.tar.gz https://raw.githubusercontent.com/Gloory1/SuperWIFI/main/superwifi-bash-v5.5.2.tar.gz
+tar -xzf superwifi-bash-v5.5.2.tar.gz
+cd superwifi-bash-v5 || exit 1
+chmod +x update.sh
+echo "1" | ./update.sh
+echo "OTA Update to v5.5.2 completed successfully. Rebooting system to apply changes..."
+sleep 2
+reboot
